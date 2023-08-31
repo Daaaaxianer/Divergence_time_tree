debfd6daa454c51d0c21765c981904b5b3376a1d
xianer <63194738+Daaaaxianer@users.noreply.github.com>
2023-09-01 00:03:21 +0800

    add tree func

--- phyTree.py
+++ phyTree.py
@@ -4,6 +4,7 @@
 # @Author   :Xian
 
 import re
+from Bio import AlignIO
 import argparse
 
 def cleanTree(intree,outtree):
@@ -14,10 +15,43 @@ def cleanTree(intree,outtree):
         print(replaceTree)
     with open(outtree, 'w') as outtr:
         outtr.write(replaceTree)
+def converttree(intree,formatin,outtree,formatout):
+    '''Convert tree format'''
+    AlignIO.convert(intree,formatin,outtree,formatout)
+
+def nwkTimeMultiply(intree,outtree):
+    '''The time of the ultrametric tree multiplied by 100'''
+    with open(intree,'r') as intr:
+        treein = intr.read()
+        arra = re.split(r',',treein)
+        arrb = []
+        for tra in arra:
+            print (f"a split : {tra}")
+            trb = re.split(r':',tra)
+            # print (f"b split : {trb}")
+            trb[-1] = str(float(trb[-1]) * 100)
+            # print(f"b split 1 : {trb[1]}")
+            arrb.append(":".join(trb))
+        treeout = ",".join(arrb)
+    with open(outtree, 'w') as outtr:
+        outtr.write(treeout)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="用途：操作进化树")
-    parser.add_argument("intree", type=str,help="input tree file in nwick formate (type = str)")
-    parser.add_argument("outtree", type=str, help="output tree file in nwick formate (type = str)")
+    parser.add_argument("-ci","--cleantree", action="store_true", help="Clear the branches and node of nwick tree")
+    parser.add_argument("-cl","--converttree", action="store_true", help="Convert the format of the tree(Support: Newick, NEXUS, phyloXML and NeXML)")
+    parser.add_argument("-tm","--timemutiply", action="store_true", help="The time of the ultrametric tree(Newick) multiplied by 100)")
+    parser.add_argument("-it","--intree", type=str,help="input tree file  (type = str)")
+    parser.add_argument("-ot","--outtree", type=str, help="output tree file  (type = str)")
+    parser.add_argument("-if", "--formatin", type=str, help="format of input tree file (type = str)")
+    parser.add_argument("-of", "--formatout", type=str, help="format of output tree file (type = str)")
+
     args = parser.parse_args()
-    cleanTree(args.intree,args.outtree)
+
+    if args.cleantree:
+        cleanTree(args.intree,args.outtree)
+    if args.converttree:
+        converttree(args.intree,args.formatin,args.outtree,args.formatout)
+    if args.timemutiply:
+        nwkTimeMultiply(args.intree,args.outtree)
